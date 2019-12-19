042ce0fb1eb1551a4a0d728487f870bdd572b7f2
ryuya226 <ryuya226@arsaga.jp>
2019-12-19 18:03:53 +0900

    refactoring-scss

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ApplicationRecord
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  has_many :groups, through: :members
+  has_many :members
+  has_many :massages
+end
