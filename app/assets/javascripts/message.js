4a5c0346c5dd5930978603eef3a44e28b7d58b18
ryuya226 <ryuya226@arsaga.jp>
2020-02-02 19:32:23 +0900

    add auto update

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,30 @@
 $(function(){
+  var reloadMessages = function() {
+    last_message_id = $('.chat-main__message-list--message:last').data("message-id");
+    $.ajax({
+      url: "api/messages",
+      type: 'get',
+      dataType: 'json',
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      if (messages.length !== 0) {
+        //追加するHTMLの入れ物を作る
+        var insertHTML = '';
+        //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
+        $.each(messages, function(i, message) {
+          insertHTML += buildHTML(message)
+        });
+        //メッセージが入ったHTMLに、入れ物ごと追加
+        $('.chat-main__message-list').append(insertHTML);
+        $('.chat-main__message-list').animate({ scrollTop: $('.chat-main__message-list')[0].scrollHeight});
+      }
+    })
+    .fail(function() {
+      console.log('error');
+    });
+  };
+
   function buildHTML(message){
     if ( message.image ) {
       var html =
@@ -60,5 +86,9 @@ $(function(){
     .always(function(){
       $('.chat-main__message-form--submit-btn').prop('disabled', false);
     });
-  })
+  });
+  
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+    setInterval(reloadMessages, 7000);
+  }
 });
